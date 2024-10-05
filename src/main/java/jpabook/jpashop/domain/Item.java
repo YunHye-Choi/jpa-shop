a293ad7070757c3a6f41ff801e13f3cfa891ea66
YunHye-Choi <anffl98@gmail.com>
2024-10-06 00:15:12 +0900

    실전 예제 3 - 다양한 연관관계 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,9 +1,10 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ManyToAny;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Item {
@@ -13,7 +14,8 @@ public class Item {
     private String name;
     private int price;
     private int stockQuantity;
-
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
     public Item() {
     }
 
