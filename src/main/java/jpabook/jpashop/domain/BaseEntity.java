6cd42947294692c30f1cb293f726a5a0dcbf550d
YunHye-Choi <anffl98@gmail.com>
2024-10-09 18:52:00 +0900

    실전 예제 4 - 상속관계 매핑

---
+++ src/main/java/jpabook/jpashop/domain/BaseEntity.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+    private String createdBy;
+    private String updatedBy;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime updatedDateTime;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public LocalDateTime getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    public void setUpdatedDateTime(LocalDateTime updatedDateTime) {
+        this.updatedDateTime = updatedDateTime;
+    }
+}
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class OrderItem {
+public class OrderItem extends BaseEntity {
     @Id @GeneratedValue
     @Column(name="ORDER_ITEM_ID")
     private Long id;
